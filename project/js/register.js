2c15df700a9052fdba8878d6975d46abcb1911be
longlong <785159060@qq.com>
2016-10-13 14:20:19 +0800

    newresetbug_code

--- project/js/register.js
+++ project/js/register.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	// 生成验证码
 	var code1 = "";//全局变量
 	function createCode(){
+		code1 = "";
 		var selectChar = new Array('0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');		
 		for (i=0;i<4;i++) {
 			var charIndex = Math.floor(Math.random()*36); 
